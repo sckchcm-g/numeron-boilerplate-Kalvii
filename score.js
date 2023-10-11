12e2cfabde75d0e30867c9f0bd1f9b1621103298
sg05c <141105618+sg05c@users.noreply.github.com>
2023-10-11 14:46:08 +0530

    donre

--- score.js
+++ score.js
@@ -1,1 +1,19 @@
 // Iteration 5: Store the player score and display it on the game over screen
+
+let score = localStorage.getItem("score");
+
+var scoreBoard = document.getElementById("score-board");
+
+var playAgainButton = document.getElementById("play-again-button");
+
+scoreBoard.innerHTML = score;
+
+playAgainButton.onclick = () => {
+    location.href = "./game.html";
+};
+
+
+
+
+
+
